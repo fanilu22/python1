c77ddf8525ad74126c3091917e0b366ca9500dc7
fanilu22 <101892729+fanilu22@users.noreply.github.com>
2022-06-28 12:00:11 -0500

    Add files via upload

---
+++ ejemplo1.py
@@ -0,0 +1,21 @@
+print("Hola")
+Nombre= input('¿como te llamas?')
+Nombre
+print("mucho gusto ", Nombre)
+
+
+edad = int(input ("edad"))
+edad
+print("tu edad es ", edad)
+
+numero1=float(input("Introduce el número uno: "))
+numero2=float(input("Introduce el numero dos: "))
+suma=numero1+numero2
+print("La suma es: ", suma)
+
+num1=float(input("Introduce el número 1:"))
+num2=float(input("Introduce el número 2:"))
+print("La suma es", num1+num2)
+
+
+
